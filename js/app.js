f59c8f982d79bd923e739a7e1e53bed4f64e8889
FahadAChaudhry <sh.fahadashraf@gmail.com>
2017-06-09 10:22:33 +0500

    visuals added

--- js/app.js
+++ js/app.js
@@ -24,6 +24,26 @@ myApp.controller('ctrl', function($scope, $http) {
         author: 'Talha Naveed',
         timestamp: '26th May, 2017 at 2:27 AM'
     });
+    $scope.list.push({
+        quote: "Main tab tak azad nai ho sakta jb tak mje apni hado ka na pata ho, apni hado sy piyar na ho wo hadain jo Allah ny bnai, wo hadain jo mere nai S.A.W chor kar gaye. Jab tak muje qanon tang karte rahe gaye main azad mehsoos nahi kar sako ga. Azadi une he mehsos hoti hai jo muqarar hado main rehna pasand karte hain. jiny Allah kay hukam se piyar hota hy.",
+        author: 'Talha Naveed',
+        timestamp: '26th May, 2017 at 2:27 AM'
+    });
+    $scope.list.push({
+        quote: "Main tab tak azad nai ho sakta jb tak mje apni hado ka na pata ho, apni hado sy piyar na ho wo hadain jo Allah ny bnai, wo hadain jo mere nai S.A.W chor kar gaye. Jab tak muje qanon tang karte rahe gaye main azad mehsoos nahi kar sako ga. Azadi une he mehsos hoti hai jo muqarar hado main rehna pasand karte hain. jiny Allah kay hukam se piyar hota hy.",
+        author: 'Talha Naveed',
+        timestamp: '26th May, 2017 at 2:27 AM'
+    });
+    $scope.list.push({
+        quote: "Main tab tak azad nai ho sakta jb tak mje apni hado ka na pata ho, apni hado sy piyar na ho wo hadain jo Allah ny bnai, wo hadain jo mere nai S.A.W chor kar gaye. Jab tak muje qanon tang karte rahe gaye main azad mehsoos nahi kar sako ga. Azadi une he mehsos hoti hai jo muqarar hado main rehna pasand karte hain. jiny Allah kay hukam se piyar hota hy.",
+        author: 'Talha Naveed',
+        timestamp: '26th May, 2017 at 2:27 AM'
+    });
+    $scope.list.push({
+        quote: "Main tab tak azad nai ho sakta jb tak mje apni hado ka na pata ho, apni hado sy piyar na ho wo hadain jo Allah ny bnai, wo hadain jo mere nai S.A.W chor kar gaye. Jab tak muje qanon tang karte rahe gaye main azad mehsoos nahi kar sako ga. Azadi une he mehsos hoti hai jo muqarar hado main rehna pasand karte hain. jiny Allah kay hukam se piyar hota hy.",
+        author: 'Talha Naveed',
+        timestamp: '26th May, 2017 at 2:27 AM'
+    });                
 
 
 });
